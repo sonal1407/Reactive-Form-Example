b5c9b801832e7a199f20067ea615da3c9c8201ec
unknown <ronakspatel1234@gmail.com>
2018-11-15 17:09:57 +0530

    feat-reactive-form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -26,19 +26,19 @@ export class ReactiveFormComponent implements OnInit {
     this.charecterRegEx = '^[a-zA-Z \-\']+';                
     this.emailRegEx = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$';
     this.numberRegEx = '^[0-9]*$';
-  }
-
-  ngOnInit() {
     
-    // Create the validation for Reactive form
-    this.reactiveForm = this.fb.group({
+     // Create the validation for Reactive form
+     this.reactiveForm = this.fb.group({
       firstName: ['', [ Validators.required, Validators.minLength(5),Validators.pattern(this.charecterRegEx)]],
       lastName: ['', [ Validators.required, Validators.maxLength(8),Validators.pattern(this.charecterRegEx)]],
       email: ['', [ Validators.required, Validators.pattern(this.emailRegEx)]],
       age : [{ value: 25, disabled: true}],
       phoneNumber: ['', [ Validators.required, Validators.minLength(10),Validators.pattern(this.numberRegEx)]]
     });
+
   }
+
+  ngOnInit() { }
 
   // Create method for submit form
   addData(): void {
